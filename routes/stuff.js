283d7d2bece0314d658009f139c5b951dd1a299a
Dammiieenn <157717253+Dammiieenn@users.noreply.github.com>
2024-05-02 11:13:45 +0200

    ok

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const stuffCtrl = require(`../controllers/stuff`);
+const stuffCtrl = require('../controllers/stuff');
 
-router.post(`/`, stuffCtrl.createThing);
-router.put('/:id', stuffCtrl.createThing);
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
 router.delete('/:id', stuffCtrl.deleteThing);
-router.get(`/:id`, stuffCtrl.getOneThing);
-router.get('/', stuffCtrl.getAllThings);
 
-   module.exports = router;  
+module.exports = router;
